ea3866670fc53e8af4124e23c872c3303ae15005
HariRaagavTR <harirams01@gmail.com>
2022-04-16 22:11:14 +0530

    Incorporate question type prediction

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from flask import Flask, request, render_template, redirect, url_for
 
-import BooleanQAModel
+from QuestionType import QuestionType
+from BooleanQAModel import BooleanQAModel
 # import NumberModel
 # import TextModel
-
-app = Flask(__name__)
-
-booleanQAModel = BooleanQAModel.BooleanQAModel('./resources/models/BooleanQAModel.pth')
-# numberModel = NumberModel()
-# textModel = TextModel()
 
 data = {
     'context': '',
     'question': '',
     'answer': 'Answer will appear here...'
 }
+
+questionType = QuestionType()
+booleanQAModel = BooleanQAModel('./resources/models/BooleanQAModel.pth')
+# numberModel = NumberModel()
+# textModel = TextModel()
+
+app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -24,9 +26,19 @@ def index():
 def submit():
     context = request.form.get("context")
     question = request.form.get("question")
+
+    qType = questionType.predict(question)
+    if qType == 'boolean':
+        answer = booleanQAModel.predict(context, question)
+    elif qType == 'numerical':
+        answer = ''
+    else:
+        answer = ''
+    
     data['context'] = context
     data['question'] = question
-    data['answer'] = booleanQAModel.predict(context, question)
+    data['answer'] = answer
+    
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
