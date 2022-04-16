ca145e41d54f89b3e1b0b797e2825a4468cd6afb
HariRaagavTR <harirams01@gmail.com>
2022-04-16 22:10:41 +0530

    Add QuestionType.py

---
+++ QuestionType.py
@@ -0,0 +1,28 @@
+'''
+TBDL: Train a model that predicts question type.
+'''
+
+class QuestionType:
+    def __init__(self):
+        self.boolenTypes = ['am', 'is', 'are', 'was', 'were', 'can',
+                            'could', 'may', 'might', 'shall', 'should',
+                            'will', 'would', 'must', 'ought', 'do',
+                            'does', 'did', 'have', 'has', 'had']
+        # Add more here!
+
+    def __isBoolean(self, question):
+        if question.split()[0].lower() in self.boolenTypes:
+            return True
+        return False
+    
+    def __isNumerical(self, question):
+        # Add logic here
+        return False
+
+    def predict(self, question):
+        if self.__isBoolean(question):
+            return 'boolean'
+        elif self.__isNumerical:
+            return 'numerical'
+        else:
+            return 'textual'
